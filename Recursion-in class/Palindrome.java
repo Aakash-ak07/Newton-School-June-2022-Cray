9cbd07c207f0ebc4f776668e4a76f6a0dc710687
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-08-30 17:23:11 +0530

    Create Palindrome.java

---
+++ Recursion-in class/Palindrome.java
@@ -0,0 +1,21 @@
+static boolean check_Palindrome(String str,int s, int e) 
+{ 
+    if(e == s)
+        return true; 
+    
+    if ((str.charAt(s)) != (str.charAt(e)))
+        return false;
+
+    if(s < e + 1)
+        return check_Palindrome(str, s + 1, e - 1);
+        return true;
+}
+
+// Sample Input
+// 2
+// ab
+// aba
+
+// Sample Output
+// false
+// true
