1eb44163698d1b7d1e68a12cac883e3393a74bef
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-03 16:19:14 +0530

    Create Number dilemma.java

---
+++ Basics of Programming, Java Programming -in class - post class/Number dilemma.java
@@ -0,0 +1,12 @@
+// Nobita likes a number if it is stored in an integer while Doraemon likes it when it is stored in a String. Your task is to write a code so that they can easily convert an integer to a string or a string to an integer whenever they want.
+
+
+static int StringToInt(String S){
+//Enter your code here Number dilemma
+return Integer.parseInt(S);
+}
+
+static String IntToString(int N){
+//Enter your code here
+return Integer.toString(N); 
+}
