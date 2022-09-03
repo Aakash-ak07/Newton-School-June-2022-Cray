a080ba18345404ade307468d6ca51748ceca872a
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-03 16:20:00 +0530

    Create Area of Square.java

---
+++ Basics of Programming, Java Programming -in class - post class/Area of Square.java
@@ -0,0 +1,18 @@
+// Given a side of a square, your task is to calculate its area.
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+      int side, area;
+      Scanner sc = new Scanner(System.in);
+      side = sc.nextInt();
+      area = side* side;
+      System.out.println(area);
+   
+    }
+}
