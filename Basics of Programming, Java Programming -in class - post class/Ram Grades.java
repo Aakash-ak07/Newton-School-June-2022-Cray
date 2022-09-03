6a7219c3c6e7466f1c13d4a0c8d39cd100ced9d8
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-03 16:20:51 +0530

    Create Ram Grades.java

---
+++ Basics of Programming, Java Programming -in class - post class/Ram Grades.java
@@ -0,0 +1,25 @@
+// Ram is studying in Class V and has four subjects, each subject carry 100 marks. He passed with flying colors in his exam, 
+// but when his neighbour asked how much percentage did he got in exam, he got stuck in calculation. 
+// Ram is a good student but he forgot how to calculate percentage. Help Ram to get him out of this problem.
+
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+      
+
+      Scanner sc = new Scanner(System.in);
+      int a= sc.nextInt();
+      int b= sc.nextInt();
+      int c= sc.nextInt();
+      int d= sc.nextInt();
+
+      System.out.println(((a+b+c+d)/4));
+                     
+    }
+}
