acf3fae84407732b0a8e174a23de9f2d7768f416
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-03 17:15:44 +0530

    Create Loop debugging 14.java

---
+++ Loops - Post Class/Loop debugging 14.java
@@ -0,0 +1,21 @@
+// Given an integer N, your task is to print all the even integer from 1 to N.
+
+
+public static void For_Loop(int n){
+        for(int i=2;i<=n;i++){
+            System.out.print(i+" ");
+            i++;
+        }
+    }
+
+// Sample Input:-
+// 5
+
+// Sample Output:-
+// 2 4
+
+// Sample Input:-
+// 6
+
+// Sample Output:-
+// 2 4 6
