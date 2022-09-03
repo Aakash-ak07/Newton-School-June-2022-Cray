9dc7b64552571fc6db05fd8b6313fe8f90168150
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-03 16:18:37 +0530

    Create String Typecast.java

---
+++ Basics of Programming, Java Programming -in class - post class/String Typecast.java
@@ -0,0 +1,8 @@
+// Given an integer N, you need to typecast this integer to String. If the typecasting is done successfully then we will print "Nice Job" otherwise "Wrong answer".
+
+static String checkConevrtion(int a)
+ {
+       // your code here
+       String s=Integer.toString(a);
+       return String.valueOf(a);
+    }
