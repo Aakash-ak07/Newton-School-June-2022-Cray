c3a81a49a44b2ec49fa622d486bb0439ba4f4f97
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-05 00:51:36 +0530

    Separating Negative and Positive numbers.java

--- Contest Question/Separating Negative and Positive numbers.java
+++ Contest Question/Separating Negative and Positive numbers.java
@@ -75,3 +75,29 @@ class Main {
 
 // Output:
 // -1 -4 -6 1 2 5
+
+
+
+
+//method 2 - Using Sliding Window with two pointer technique
+// Time Complexity: O(n*window)
+// Auxiliary Space: O(1)
+
+public static void rearrangePosNegWithOrder(int[] arr)
+   {
+       int i = 0, j = 0;
+       while (j < arr.length) {
+           if (arr[j] >= 0) {
+               j++;
+           }
+           else {
+               for (int k = j; k > i; k--) {
+                   int temp = arr[k];
+                   arr[k] = arr[k - 1];
+                   arr[k - 1] = temp;
+               }
+               i++;
+               j++;
+           }
+       }
+   }
