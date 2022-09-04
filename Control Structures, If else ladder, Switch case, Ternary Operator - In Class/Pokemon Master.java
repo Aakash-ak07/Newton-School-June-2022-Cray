093eb5a61ffcd8eeec70c042998a67f2c58ecd34
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-04 13:05:06 +0530

    Create Pokemon Master.java

---
+++ Control Structures, If else ladder, Switch case, Ternary Operator - In Class/Pokemon Master.java
@@ -0,0 +1,20 @@
+// Sara has A Pokeball with her and there are B pokemon in front of Sara. Considering each pokemon takes one Pokeball, your task is to tell Sara if she can catch all the pokemon or not.
+
+
+static int PokemonMaster(int A, int B){
+//Enter your code here
+
+return (A>=B)?1:0;
+}
+
+// Sample Input:-
+// 4 3
+
+// Sample Output:-
+// 1
+
+// Sample Input:-
+// 4 6
+
+// Sample Output:-
+// 0
