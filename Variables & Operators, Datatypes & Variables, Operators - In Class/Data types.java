84748c160e638948d75887fc907307a969a86144
Aakash <111000108+Aakash-ak07@users.noreply.github.com>
2022-09-04 13:17:08 +0530

    Create Data types.java

---
+++ Variables & Operators, Datatypes & Variables, Operators - In Class/Data types.java
@@ -0,0 +1,23 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+                      
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        long b=sc.nextLong();
+        float c=sc.nextFloat();
+        double d=sc.nextDouble();
+        char e=sc.next().charAt(0);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.printf("%.2f\n",c);
+        System.out.printf("%.4f\n",d);
+        System.out.print(e);
+    }
+}
